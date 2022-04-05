1af68d261e80b6e31fe1bb66b80edfb439b6c517
Dmitry Ryabchun <dmitry.ryabchun@agilie.com>
2022-04-06 00:14:49 +0300

    added ability to add and get a list of media files for the selected chat

---
+++ build/lib/files/files.interface.d.ts
@@ -0,0 +1,10 @@
+export declare type FileID = string | number;
+export interface IFile {
+    id: number;
+    createdAt: number;
+    link: string;
+    title: string;
+    size: number;
+    format: string;
+}
+//# sourceMappingURL=files.interface.d.ts.map
